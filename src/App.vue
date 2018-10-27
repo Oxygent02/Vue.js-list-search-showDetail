5e91e63f8d3520444588df37c42f62454a6fd061
Oxygent02 <33537279+Oxygent02@users.noreply.github.com>
2018-10-27 12:19:32 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <navbar-starwars @input-changed="getInput"></navbar-starwars>
+    <float-button  @character-added="showCharacters"></float-button>
+    <character-list :characters="characters" :search="search"></character-list>
+  </div>
+</template>
+
+<script>
+import navbarStarwars from "./components/navbarStarwars";
+import floatButton from "./components/floatButton";
+import characterList from "./components/characterList";
+
+export default {
+  name: "App",
+  components: {
+    navbarStarwars,
+    floatButton,
+    characterList
+  },
+  data(){
+    return{
+      characters: [],
+      search: ""
+    }
+  },
+  methods:{
+    showCharacters(payload){
+      this.characters = payload;
+    },
+    getInput(data){
+      this.search = data
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: 100vh;
+  position: relative;
+}
+</style>
---
+++ src/components/navbarStarwars.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <nav>
+      <div class="nav-wrapper">
+        <form>
+          <div class="input-field">
+            <input type="search" v-model="search" @change="doSearch">
+            <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+            <i class="material-icons">close</i>
+          </div>
+        </form>
+      </div>
+    </nav>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      search : ""
+    };
+  },
+  methods: {
+    doSearch(){
+      this.$emit("input-changed", this.search);
+    }
+  }
+};
+</script>
